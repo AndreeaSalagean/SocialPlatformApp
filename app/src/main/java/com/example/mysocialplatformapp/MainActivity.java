621d36debcc418581ed00d92ac1b55c7cd923cf1
AndreeaSalagean <37187103+AndreeaSalagean@users.noreply.github.com>
2019-12-10 16:44:33 +0200

    Main page

--- app/src/main/java/com/example/mysocialplatformapp/MainActivity.java
+++ app/src/main/java/com/example/mysocialplatformapp/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.mysocialplatformapp;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button services;
+    private Button announcements;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("hello");
+
+        services= findViewById(R.id.servicesB);
+        announcements = findViewById(R.id.announcementsB);
+
+//        final Intent intent = new Intent ( this, MainScreenActivity.class);
+
+        services.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                startActivity(intent);
+            }
+        });
+
+        announcements.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                startActivity(intent);
+            }
+        });
     }
 }
