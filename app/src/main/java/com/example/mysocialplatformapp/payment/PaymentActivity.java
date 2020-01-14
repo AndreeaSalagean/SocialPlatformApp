d9b6505357a5c6e7226f99c42f7e988cbd6e693a
dlauran <laurandarius@gmail.com>
2020-01-14 16:53:08 +0200

    Create payment layout.

---
+++ app/src/main/java/com/example/mysocialplatformapp/payment/PaymentActivity.java
@@ -0,0 +1,24 @@
+package com.example.mysocialplatformapp.payment;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.mysocialplatformapp.R;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner6);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(PaymentActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.months));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+    }
+}
